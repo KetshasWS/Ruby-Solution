1a94c20ebe471c2b5e624292d974f0e45910f77a
Ketshas Web Solutions <ketshas@gmail.com>
2013-02-11 03:16:30 -0800

    Create SelectStatementFormData.rb

---
+++ SelectStatementFormData.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+!include
+filename: database.yml
+filename: FormData.rb
+
+begin
+
+    rs = con.query("SELECT * FROM Data WHERE FName='FN' OR LName='LN'")
+    
+   fields = rs.fetch_fields
+    puts "%3s %s" % [fields[0].name, fields[1].name]
+
+    rs.each_hash do |row|
+        puts "%3s %s" % [row['FName'], row['LName']]
+    end
+
+rescue Mysql::Error => e
+    puts e.errno
+    puts e.error
+    
+ensure
+    con.close if con
+end
